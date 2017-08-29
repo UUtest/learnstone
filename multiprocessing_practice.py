3f4ed090dcbf5e6deed2577ac258d689a6e34908
mac <740405000@qq.com>
2017-08-29 19:46:10 +0800

    commplte multiprocessing.py again

--- multiprocessing_practice.py
+++ multiprocessing_practice.py
@@ -7,8 +7,8 @@ def run_proc(name):
     print('Run child process %s (%s)...' % (name, os.getpid()))
 if __name__ == '__main__':
     print('Parent process %s.' % os.getpid())
-    p = Process(target=run_proc, args=('test',))
+    p = Process(target=run_proc, args=('test',))#传入执行函数和 函数的参数，创建出一个实例
     print('Child process will start.')
-    p.start()
-    p.join()
+    p.start()                                   #用start方法启动
+    p.join()                                    #join方法可以等待子进程结束后再继续往下运行，，常用于进程间的同步
     print('Child process end.')
